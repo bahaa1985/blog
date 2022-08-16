5a52728c1fa980dd822b00dea65955e192e20850
bahaa1985 <bahaa_salaheldin@live.com>
2022-08-16 21:42:18 +0200

    db connect

--- pages/api/form.js
+++ pages/api/form.js
@@ -1,4 +1,23 @@
-export default function Form(req,res){
+let mongoClient=require('mongodb').MongoClient
+let url='mongodb+srv://bahaa1985:2640077@cluster0.nes5a.mongodb.net/?retryWrites=true&w=majority'
+
+
+export default async function Form(req,res){
+    await mongoClient.connect(url,(err,db)=>{
+        if(err) throw err  
+        let db1= db.db('Teams')
+        console.log('db name: ',db1.databaseName)
+        db1.collection("Teams").find({name: 'Al ahly'}).toArray().then((result)=>{
+            // if(err) console.log('err',err)
+            console.log('collections:',result)
+            db.close()
+        }).catch((err)=>{
+            console.lo('error:',err)
+        })
+       
+        
+    })
+    
     let fname=req.body.fname
     let lname=req.body.lname
     res.json(fname+' '+lname)
