42c55f19a845e10cc45e413ede6bd07d570aacfe
bahaa1985 <bahaa_salaheldin@live.com>
2022-08-16 22:59:29 +0200

    insertr commit

--- pages/api/form.js
+++ pages/api/form.js
@@ -2,23 +2,31 @@ let mongoClient=require('mongodb').MongoClient
 let url='mongodb+srv://bahaa1985:2640077@cluster0.nes5a.mongodb.net/?retryWrites=true&w=majority'
 
 
+
 export default async function Form(req,res){
+    let fname=req.body.fname
+    let count=req.body.lname
+    // res.json(fname+' '+lname)
     await mongoClient.connect(url,(err,db)=>{
         if(err) throw err  
         let db1= db.db('Teams')
         console.log('db name: ',db1.databaseName)
-        db1.collection("Teams").find({name: 'Al ahly'}).toArray().then((result)=>{
-            // if(err) console.log('err',err)
-            console.log('collections:',result)
-            db.close()
-        }).catch((err)=>{
-            console.lo('error:',err)
-        })
-       
+        // db1.collection("Teams").find({name: 'Al ahly'}).toArray().then((result)=>{
+        //     // if(err) console.log('err',err)
+        //     console.log('collections:',result)
+        //     db.close()
+        // }).catch((err)=>{
+        //     console.lo('error:',err)
+        // })
+       let myObj={name:fname,titles:count}
+       db1.collection("Teams").insertOne(myObj,(err,result)=>{
+        if (err) throw err
+        // console.log(fname +" document inserted");
+        res.json(result)
+        db.close()
+       })
         
     })
     
-    let fname=req.body.fname
-    let lname=req.body.lname
-    res.json(fname+' '+lname)
+   
 }
